10f06015f1dc96ba5c7ca52511d4af4491911f85
jk <jiakeqi@icloud.com>
2016-11-26 14:02:51 +0800

    change config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports.host = function () {
   const isProd = process.env.NODE_ENV === 'production'
-  const port = process.env.PORT || '8080'
+  const port = process.env.PORT || '4000'
   const devHost = 'http://127.0.0.1:' + port
   const prodHost = 'http://jk77.me:' + port
   const host = isProd ? prodHost : devHost
